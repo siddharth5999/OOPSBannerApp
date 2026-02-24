5304f14e79fbcf944c8c51d86ddc5faba562af7f
Siddharth <sidd.narain@gmail.com>
2026-02-24 10:44:02 +0530

    UseCase5

---
+++ uc5.java
@@ -0,0 +1,20 @@
+
+public class OopsBanner {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+            String.join("   ", "  ***  ", "  ***  ", "******", " *****"),
+            String.join("   ", " **  **", " ** ** ", "**   **", "**   **"),
+            String.join("   ", " **  **", " ** ** ", "**   **", "       "),
+            String.join("   ", " **  **", " ** ** ", "******", " **** "),
+            String.join("   ", " **  **", " ** ** ", "**     ", "     **"),
+            String.join("   ", " **  **", " ** ** ", "**     ", "      **"),
+            String.join("   ", "  ***  ", "  ***  ", "**     ", " *****")
+        };
+
+            for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}

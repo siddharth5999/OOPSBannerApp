bef2196edca02587ee8ee3e9841ef6bbd1c71443
Siddharth <sidd.narain@gmail.com>
2026-02-24 09:08:10 +0530

    UseCase2

---
+++ uc2.java
@@ -0,0 +1,11 @@
+class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   *******    ****** ");
+        System.out.println("*     *  *     *  *     *    *      ");
+        System.out.println("*     *  *     *  *******    *****  ");
+        System.out.println("*     *  *     *  *              *  ");
+        System.out.println(" *****    *****   *         ******  ");
+    }
+}
